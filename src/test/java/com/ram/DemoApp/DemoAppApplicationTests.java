3b17cefccf42928e562eccff2d70499f3ff05599
Ram <ramji.aqua@gmail.com>
2024-10-22 22:58:14 +0530

    Initial Commit.

---
+++ src/test/java/com/ram/DemoApp/DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ram.DemoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
